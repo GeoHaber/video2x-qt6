6bb5efd887e8d13bc6170269fac0194f295fe91f
k4yt3x <i@k4yt3x.com>
2023-07-05 03:25:49 +0000

    added main and init files
    Signed-off-by: k4yt3x <i@k4yt3x.com>

---
+++ video2x_qt6/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .video2x_qt6 import main
---
+++ video2x_qt6/__main__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .video2x_qt6 import main
+
+if __name__ == "__main__":
+    main()
